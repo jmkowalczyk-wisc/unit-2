433f5b37000ff9d97e80a40cc5215d1b5c452a52
Joseph Kowalczyk <jmkowalczyk@wisc.edu>
2026-02-22 14:50:59 -0600

    Minor code cleanup, mainly sorting main.js into functions.

--- activity-5/js/main.js
+++ activity-5/js/main.js
@@ -1,41 +1,52 @@
-// Initialize the map, set its view to coordinates [51.505, -0.09] and zoom level 13
-var map = L.map('map').setView([0, 0], 2);
+// Initialize the map, set its view to coordinates [51.505, -0.09] and zoom level 2
+var map;
 
-// Add a tile layer basemap using OpenStreetMap tiles, set maximum zoom to 19, and attribute OpenStreetMap.
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map); // Adds the tile layer to the map. .addTo(map) will always be used to add a feature to the map.
+function mapInit(){
+    map = L.map('map').setView([0, 0], 2);
 
-// Fetch data with AJAX, then initialize a styling variable, then add a geojson leaflet layer.
-fetch("data/ElectricityGenRenewPercent.geojson")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        var geojsonMarkerStyle = {
-            radius: 8,
-            fillColor: '#dd0',
-            color: '#000',
-            weight: 1,
-            fillOpacity: 0.5
+    // Add a tile layer basemap using OpenStreetMap tiles, and attribute OpenStreetMap.
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map); // Adds the tile layer to the map. .addTo(map) will always be used to add a feature to the map.
+
+    // Call addData, loading Lab 1 data and creating markers.
+    addData(map);
+}
+
+function addData(map){
+    // Fetch data with AJAX, then initialize a styling variable, then add a geojson leaflet layer.
+    fetch("data/ElectricityGenRenewPercent.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            var geojsonMarkerStyle = {
+                radius: 8,
+                fillColor: '#dd0',
+                color: '#000',
+                weight: 1,
+                fillOpacity: 0.5
+            };
+
+            L.geoJson(json, { // Create a geojson layer, use pointToLayer to add the points to the map, then uses onEachFeature to add popups.
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerStyle);
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        });
+
+    // Add popups to each feature from MegaCities.geojson
+    function onEachFeature(feature, layer) {
+        // No dedicated popup content exists in the geojson, we need to create an html string to store the data
+        var popupContent = "";
+        if (feature.properties) { // If the feature has properties
+            for (var property in feature.properties) { // For each property in the feature
+                popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>"; // Adds a string like "<p>property: value</p> to the html for the popup"
+            };
+            layer.bindPopup(popupContent); // Bind the popup to the feature
         };
-
-        L.geoJson(json, { // Create a geojson layer, use pointToLayer to add the points to the map, then uses onEachFeature to add popups.
-            pointToLayer: function (feature, latlng){
-                return L.circleMarker(latlng, geojsonMarkerStyle);
-            },
-            onEachFeature: onEachFeature
-        }).addTo(map);
-    });
-
-// Add popups to each feature from MegaCities.geojson
-function onEachFeature(feature, layer) {
-    // No dedicated popup content exists in the geojson, we need to create an html string to store the data
-    var popupContent = "";
-    if (feature.properties) { // If the feature has properties
-        for (var property in feature.properties) { // For each property in the feature
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>"; // Adds a string like "<p>property: value</p> to the html for the popup"
-        };
-        layer.bindPopup(popupContent); // Bind the popup to the feature
     };
 };
+
+document.addEventListener('DOMContentLoaded', mapInit)
